1327f1036ebaaa4671880bf8e5bf662dea9e8e69
Ahmad Valiev <gracioso@pm.me>
2025-04-25 18:10:00 +0300

    Fix

--- core/database.py
+++ core/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from core.config import get_settings
+from core.config import load_yaml_config
 
-settings = get_settings()
+yaml_config = load_yaml_config()
+db = yaml_config["database"]
+db_url = f"{db['driver']}://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['name']}"
 
 # Синхронное подключение (используется в большинстве кода)
-engine = create_engine(settings.get_database_url)
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
